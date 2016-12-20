b494fec912c7ff39cc5e8d46b3189d9f41cde7a4
Elizabeth Rogers <beth3346@gmail.com>
2016-12-20 10:09:09 -0600

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('scsslint', function() {
-    return gulp.src(`src/**/*.scss`)
+    return gulp.src('src/**/*.scss')
         .pipe(scsslint());
 });
 
 gulp.task('default', ['scsslint'], function() {
-    gulp.watch(`src/**/*.scss`, ['scsslint']);
+    gulp.watch('src/**/*.scss', ['scsslint']);
 });
 
 gulp.task('build', ['scsslint']);
